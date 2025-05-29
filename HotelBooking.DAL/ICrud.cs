eafc7670b29a5be6635c7330f9d5d31963cfed39
Starinin Andrey <starinin-andrey@ya.ru>
2025-05-29 21:10:56 +0300

    Добавление TableRooms

---
+++ HotelBooking.DAL/ICrud.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HotelBooking.Models;
+
+namespace HotelBooking.DAL;
+
+public interface ICrudAsync<T> 
+    where T : IModel
+{
+    public IAsyncEnumerable<T> GetAllAsync();
+    public Task<T> GetByIdAsync(int id);
+    public Task<T> CreateAsync(T entity);
+    public Task<T> UpdateAsync(T entity);
+    public Task DeleteAsync(int id);
+}
---
+++ HotelBooking.DAL/TableRooms.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using HotelBooking.Models;
+using Npgsql;
+
+namespace HotelBooking.DAL;
+
+public class TableRooms : ICrudAsync<Room>
+{
+    private readonly NpgsqlConnection _connection;
+    
+    public TableRooms(string connectionString)
+    {
+        if (string.IsNullOrWhiteSpace(connectionString)) 
+            throw new ArgumentNullException("Строка подключения не может быть пустой!");
+        
+        _connection = new NpgsqlConnection(connectionString);
+    }
+    
+    public IAsyncEnumerable<Room> GetAllAsync()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public async Task<Room> GetByIdAsync(int id)
+    {
+        var sql = "SELECT * FROM table_rooms WHERE id = @id";
+        return await _connection.QuerySingleAsync<Room>(sql, new { id });
+    }
+
+    public Task<Room> CreateAsync(Room entity)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<Room> UpdateAsync(Room entity)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+}
