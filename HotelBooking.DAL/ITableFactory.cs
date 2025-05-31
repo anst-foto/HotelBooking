094022fe8374dc093aebee9513b4c069e11d61a2
Starinin Andrey <starinin-andrey@ya.ru>
2025-05-31 21:01:35 +0300

    Создание фабрики для TableRooms

---
+++ HotelBooking.DAL/ITableFactory.cs
@@ -0,0 +1,11 @@
+﻿using HotelBooking.Models;
+
+namespace HotelBooking.DAL;
+
+public interface ITableFactory<out T, T1> 
+    where T : ICrudAsync<T1> 
+    where T1 : IModel
+{
+    public string? ConnectionString { get; }
+    public T Create(string fileName, string connectionStringName);
+}
---
+++ HotelBooking.DAL/TableRoomsFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using HotelBooking.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace HotelBooking.DAL;
+
+public class TableRoomsFactory : ITableFactory<TableRooms, Room>
+{
+    public string? ConnectionString { get; private set; }
+    public TableRooms Create(
+        string fileName = "appsettings.json", 
+        string connectionStringName = "DefaultConnection")
+    {
+        if (string.IsNullOrWhiteSpace(fileName)) 
+            throw new ArgumentNullException(nameof(fileName));
+        if (string.IsNullOrWhiteSpace(connectionStringName)) 
+            throw new ArgumentNullException(nameof(connectionStringName));
+
+        
+        try
+        {
+            ConnectionString = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(fileName)
+                .Build()
+                .GetConnectionString(connectionStringName);
+        }
+        catch (Exception e)
+        {
+            throw new ConfigurationException("Ошибка с конфигурацией", e);
+        }
+        
+        if (string.IsNullOrEmpty(ConnectionString)) 
+            throw new ConfigurationException("Строка подключения пуста");
+        
+        return new TableRooms(ConnectionString);
+    }
+
+    /*public static TableRooms Create()
+    {
+        var factory = new TableRoomsFactory();
+        return factory.Create();
+    }*/
+}
