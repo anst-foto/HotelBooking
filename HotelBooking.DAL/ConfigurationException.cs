30b6119ec8274f18561898f986038dddd0bc3463
Starinin Andrey <starinin-andrey@ya.ru>
2025-05-31 21:01:10 +0300

    Создание исключений для конфигурации

---
+++ HotelBooking.DAL/ConfigurationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HotelBooking.DAL;
+
+public class ConfigurationException : Exception
+{
+    public ConfigurationException(string? message) : base(message)
+    {
+    }
+
+    public ConfigurationException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
