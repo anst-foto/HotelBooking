68d0bcee1776a8601b49543ee2d3323a27b5c3df
Starinin Andrey <starinin-andrey@ya.ru>
2025-05-31 21:04:16 +0300

    Создание теста для TableRooms

--- HotelBooking.DAL.Test/TableRoomsTest.cs
+++ HotelBooking.DAL.Test/TableRoomsTest.cs
@@ -1,9 +1,28 @@
+using System.Threading.Tasks;
+using HotelBooking.Models;
+
 namespace HotelBooking.DAL.Test;
 
 public class TableRoomsTest
 {
+    private const string FileName = "appsettings.json";
+    private const string ConnectionStringName = "Test";
+    
     [Fact]
-    public void GetByIdAsyncTest()
+    public async Task GetByIdAsyncTest()
     {
+        var expected = new Room()
+        {
+            Id = 1,
+            Number = "101",
+            IsDeleted = false
+        };
+
+        var factory = new TableRoomsFactory();
+
+        var tableRooms = factory.Create(FileName, ConnectionStringName);
+        var actual = await tableRooms.GetByIdAsync(1);
+        
+        Assert.Equal(expected, actual);
     }
 }
